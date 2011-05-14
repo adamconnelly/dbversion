7cdf9d005cb6644afa340f181d554f97ec9b0f40
Adam Connelly <adam.rpconnelly@gmail.com>
2011-05-14 23:42:35 +0100

    Added an initial implementation of IVersionProvider for a simple numeric version scheme (NumericVersionProvider). Added a console project that will become the CLI for the version library and added DatabaseCreator which will be the entry point of the MEF container and will be used to execute the database scripts.

---
+++ src/DatabaseVersion/DatabaseCreator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using DatabaseVersion.Archives;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using System.IO;
+
+namespace DatabaseVersion
+{
+    public class DatabaseCreator
+    {
+        private readonly string connectionString;
+
+        public DatabaseCreator(string pluginPath)
+        {
+            DirectoryInfo pluginPathInfo = new DirectoryInfo(pluginPath);
+            if (!pluginPathInfo.Exists)
+            {
+                pluginPathInfo.Create();
+            }
+
+            var assemblyCatalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var directoryCatalog = new DirectoryCatalog(pluginPath);
+            var aggregateCatalog = new AggregateCatalog(assemblyCatalog, directoryCatalog);
+
+            var container = new CompositionContainer(aggregateCatalog);
+            container.ComposeParts(this);
+        }
+
+        [ImportMany]
+        public IEnumerable<IDatabaseArchiveFactory> ArchiveFactories
+        {
+            get;
+            set;
+        }
+
+        [Import]
+        public IVersionProvider VersionProvider
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Creates a database at the specified version.
+        /// </summary>
+        /// <param name="version">The version of database to create.</param>
+        public void Create(string version)
+        {
+        }
+
+        public void Upgrade()
+        {
+        }
+
+        public void Upgrade(string version)
+        {
+        }
+    }
+}
---
+++ src/DatabaseVersion/Version/NumericVersionProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+namespace DatabaseVersion.Version
+{
+    [Export(typeof(IVersionProvider))]
+    public class NumericVersionProvider : IVersionProvider
+    {
+        public object CreateVersion(string versionString)
+        {
+            return int.Parse(versionString);
+        }
+
+        public bool VersionTableExists(System.Data.IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetLatestVersion(System.Data.IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateVersionTable(System.Data.IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertVersion(object version, System.Data.IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComparer<object> GetComparer()
+        {
+            return new IntComparer();
+        }
+
+        private class IntComparer : Comparer<object>
+        {
+            public override int Compare(object x, object y)
+            {
+                int intX = (int)x;
+                int intY = (int)y;
+
+                return intX - intY;
+            }
+        }
+    }
+}
