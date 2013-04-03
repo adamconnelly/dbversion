4c58ff0249c758bdc3a89f6d7debc482c212fe26
Donald Walker <>
2013-04-03 22:01:39 +0100

    Add verify command

---
+++ src/DatabaseVersion.Console/Command/Verify/VerifyCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using dbversion.Console.Command.Create;
+
+namespace dbversion.Console.Command.Verify
+{
+    [Export(typeof(IConsoleCommand))]
+    public class VerifyCommand: CreateCommand
+    {
+        public override string Name
+        {
+            get
+            {
+                return "verify";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Verify a database creation or upgrade using the specified archive.";
+            }
+        }
+
+        public override string Usage
+        {
+            get
+            {
+                return "dbversion " + this.Name + " [options]";
+            }
+        }
+
+        protected override bool Commit()
+        {
+            return false;
+        }
+    }
+}
